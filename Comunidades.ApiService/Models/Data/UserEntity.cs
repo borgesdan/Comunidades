7e1f656fedbfc6afaf88dc98aa36c02367a71b21
Danilo <borges_santos89@hotmail.com>
2024-08-13 21:09:17 -0300

    Implementa atributo Table em TableUserEntity

--- Comunidades.ApiService/Models/Data/UserEntity.cs
+++ Comunidades.ApiService/Models/Data/UserEntity.cs
@@ -1,11 +1,13 @@
 ﻿using Comunidades.ApiService.Models.Enums;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Comunidades.ApiService.Models.Data
 {
     /// <summary>
     /// Representa a tabela de dados do usuário.
     /// </summary>    
+    [Table("User")]
     public class UserEntity : BaseEntity
     {
         public const int NameLength = 256;
