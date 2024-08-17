b665781d0a5635ba91c6fbb6d8f5795908596b17
Danilo <borges_santos89@hotmail.com>
2024-08-17 15:18:36 -0300

    Implementa BearerToken

---
+++ Comunidades.ApiService/Shared/BearerToken.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Comunidades.ApiService.Shared
+{
+    public static class BearerToken
+    {
+        public static string Generate(DateTime expireDate, params Claim[] claims)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("Key");
+            var claimsIdentity = new ClaimsIdentity(claims);
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = claimsIdentity,
+                Expires = expireDate,
+                SigningCredentials = credentials
+            };
+            
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var writtenToken = tokenHandler.WriteToken(token);
+            return writtenToken;
+        }
+    }
+}
